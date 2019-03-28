1050ff3161fec68eb50b93c400bed3973dc353ce
mygit <784778060@qq.com>
2019-03-28 22:03:15 +0800

    第二张完成

--- js/main.js
+++ js/main.js
@@ -4,3 +4,18 @@ $(document).ready(function(){
 	  interval: 1000
 	});
 })
+var num =1;
+$('.shuliang .jia').click(function(){
+	num++;
+	$('.shuliang .shu').html(num);
+	
+})
+$('.shuliang .jian').click(function(){
+	
+	if(num==1){
+		num=1;
+	}else{
+		num--;
+	}
+	$('.shuliang .shu').html(num);
+})
